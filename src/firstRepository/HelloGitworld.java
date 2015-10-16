2df565af0c1b2102e5356c9433f041e3a3ef4e90
stevejun <steve.jun24@gmail.com>
2015-10-16 02:42:05 -0400

    HelloGitworld.java
    Adding HelloGitworld.java to firstRepository.

---
+++ src/firstRepository/HelloGitworld.java
@@ -0,0 +1,10 @@
+package firstRepository;
+
+public class HelloGitworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Gitworld!");
+		System.out.println("Mission Accomplished.");
+		System.out.println("Bring him home.");
+	}
+}
